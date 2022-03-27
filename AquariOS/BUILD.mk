adcdc7ea564216c471606d7168f422e9dd72a36f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-27 16:26:09 -0700

    BUILD.mk [V1]
    Version 1 of the build makefile for this project.

---
+++ AquariOS/BUILD.mk
@@ -0,0 +1,15 @@
+# Start of script
+# Build this project and optimize it before flashing to your aquarium hardware
+
+# Delete development files
+rm -f /AquariOS/!OldVersions/
+
+# Delete the makefile
+rm -f /AquariOS/BUILD.mk # Can a makefile delete itself?
+
+# File info
+# File type: GNU Make(file) (*.mk *.mak *.make *makefile)
+# File version: 1 (2022, Sunday, March 27th at 4:26 pm)
+# Line count (Including blank lines and compiler line): 16
+
+# End of script
